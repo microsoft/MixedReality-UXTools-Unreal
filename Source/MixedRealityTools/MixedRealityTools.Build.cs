8595c872b05fe4b169fa18fd092ed3474eb8025c
Luis Valverde <luval@microsoft.com>
2019-10-29 16:14:32 +0000

    Build fix: added missing module dependencies.

--- Source/MixedRealityTools/MixedRealityTools.Build.cs
+++ Source/MixedRealityTools/MixedRealityTools.Build.cs
@@ -11,16 +11,11 @@ public class MixedRealityTools : ModuleRules
 		// Required to avoid errors about undefined preprocessor macros (C4668) when building DirectXMath.h
         bEnableUndefinedIdentifierWarnings = false;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "WindowsMixedRealityHandTracking", "LiveLinkInterface" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		if (Target.bBuildEditor)
+        {
+            PrivateDependencyModuleNames.Add("UnrealEd");
+        }
 	}
 }
