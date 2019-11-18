e8936b77865855c6e3c89e214349506338148620
Luis Valverde <luval@microsoft.com>
2019-11-18 14:54:53 +0000

    Merged PR 44: Tweak button visualizer so it is more clear where the initial position is.
    Tweak button visualizer so it is more clear where the initial position is.

--- MRU_Game/Plugins/MixedRealityUtils/Source/MixedRealityUtilsEditor/Private/PressableButtonComponentVisualizer.cpp
+++ MRU_Game/Plugins/MixedRealityUtils/Source/MixedRealityUtilsEditor/Private/PressableButtonComponentVisualizer.cpp
@@ -2,6 +2,33 @@
 #include "MixedRealityUtilsEditor.h"
 #include <SceneManagement.h>
 
+namespace
+{
+	void DrawQuad(FPrimitiveDrawInterface* PDI, float Width, float Height, float PressedDistance, const FMatrix& Transform, const FLinearColor& Color, bool bDashed = false)
+	{	
+		FVector Vertices[] =
+		{
+			FVector(PressedDistance, Width, Height),
+			FVector(PressedDistance, Width, -Height),
+			FVector(PressedDistance, -Width, -Height),
+			FVector(PressedDistance, -Width, Height)
+		};
+
+		for (int i = 0; i < 4; ++i)
+		{
+			FVector Start = Transform.TransformPosition(Vertices[i]);
+			FVector End = Transform.TransformPosition(Vertices[(i + 1) % 4]);
+			if (bDashed)
+			{
+				DrawDashedLine(PDI, Start, End, Color, 0.1, SDPG_Foreground);
+			}
+			else
+			{
+				PDI->DrawLine(Start, End, Color, SDPG_Foreground, 0.05f);
+			}
+		}
+	}
+}
 
 void FPressableButtonComponentVisualizer::DrawVisualization(const UActorComponent* Component, const FSceneView* View, FPrimitiveDrawInterface* PDI)
 {
@@ -10,29 +37,17 @@ void FPressableButtonComponentVisualizer::DrawVisualization(const UActorComponen
 		FVector Extents = Button->GetComponentScale() * Button->Extents;
 		FMatrix Matrix = Button->GetComponentTransform().ToMatrixNoScale();
 		
-		// Draw movement box, from resting position to maximum push distance
-		{
-			FBox Box = FBox::BuildAABB(FVector::ForwardVector * Extents.X, Extents);
-			DrawWireBox(PDI, Matrix, Box, FLinearColor::Green, SDPG_Foreground, 0.1f);
-		}
+		// Rest position
+		DrawQuad(PDI, Extents.Y, Extents.Z, 0, Matrix, FLinearColor::White);
 
-		// Draw quad at pressed distance
-		{
-			float PressedDistance = Extents.X * Button->PressedFraction * 2;
-			FVector Vertices[] =
-			{
-				FVector(PressedDistance, Extents.Y, Extents.Z),
-				FVector(PressedDistance, Extents.Y, -Extents.Z),
-				FVector(PressedDistance, -Extents.Y, -Extents.Z),
-				FVector(PressedDistance, -Extents.Y, Extents.Z)
-			};
+		// Maximum push distance
+		const float MaxPushDistance = 2 * Extents.X;
+		FLinearColor DarkGray(.25f, .25f, .25f);
+		DrawQuad(PDI, Extents.Y, Extents.Z, MaxPushDistance, Matrix, DarkGray);
 
-			for (int i = 0; i < 4; ++i)
-			{
-				FVector Start = Matrix.TransformPosition(Vertices[i]);
-				FVector End = Matrix.TransformPosition(Vertices[(i + 1) % 4]);
-				PDI->DrawLine(Start, End, FLinearColor::Blue, SDPG_Foreground, 0.1f);
-			}
-		}
+		// Pressed distance
+		float PressedDistance = MaxPushDistance * Button->PressedFraction;
+		FLinearColor LightGray(.75f, .75f, .75f);
+		DrawQuad(PDI, Extents.Y, Extents.Z, PressedDistance, Matrix, LightGray, true);
 	}
 }
