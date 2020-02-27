1696a26ffd4dcd83cbb057900830a004324aa8dd
Jason Ray <jaray@microsoft.com>
2020-02-26 18:24:26 -0800

    Add missing include to fix android build

--- UXToolsGame/Plugins/UXTools/Source/UXTools/Private/Controls/UxtBoundingBoxManipulatorComponent.cpp
+++ UXToolsGame/Plugins/UXTools/Source/UXTools/Private/Controls/UxtBoundingBoxManipulatorComponent.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Controls/UxtBoundingBoxManipulatorComponent.h"
+#include "Components/PrimitiveComponent.h"
 #include "Engine/World.h"
 #include "GameFramework/Actor.h"
 #include "DrawDebugHelpers.h"
