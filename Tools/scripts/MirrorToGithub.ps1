5ab9bd562c99845f798976b66be0d8efa4eae5cc
Stephen Kennedy <stkenned@microsoft.com>
2020-03-20 16:04:02 +0000

    Merged PR 164: Add release branch mirroring script
    Tested with a temporary branch in https://github.com/microsoft/MixedReality-UXTools-Unreal/branches
    
    Related work items: #271

---
+++ Tools/scripts/MirrorToGithub.ps1
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Array of release branches to sync. We assume the branch name is "release/$name"
+# Comma separates each element
+$branches=@("0.1.0")
+#
+$github="https://github.com/microsoft/MixedReality-UXTools-Unreal"
+
+git fetch origin
+if($LastExitCode -ne 0) {
+    Write-Host "Unable to git fetch"
+	exit 1
+}
+foreach ($branch in $branches) {
+    Write-Host "Pushing ${branch}"
+    git push --progress "$github" "refs/remotes/origin/release/${branch}:refs/heads/release/${branch}"
+}
