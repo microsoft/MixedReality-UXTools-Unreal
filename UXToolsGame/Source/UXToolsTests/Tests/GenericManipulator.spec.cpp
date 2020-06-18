8d3a568579c3deac711ad2f504903a03485ff01c
Carl McCaffrey <Carl.McCaffrey@microsoft.com>
2020-06-18 16:18:58 +0000

    Merged PR 348: Tests for one handed far manipulation rotation
    Tests for rotation around object center and grab point for one handed far manipulation rotation.
    
    Also a general refactoring of the generic manipulator tests
    
    Related work items: #450

--- UXToolsGame/Source/UXToolsTests/Tests/GenericManipulator.spec.cpp
+++ UXToolsGame/Source/UXToolsTests/Tests/GenericManipulator.spec.cpp
@@ -4,6 +4,7 @@
 #include "Engine.h"
 #include "FrameQueue.h"
 #include "GenericManipulatorTestComponent.h"
+#include "Input/UxtFarPointerComponent.h"
 #include "Input/UxtNearPointerComponent.h"
 #include "Tests/AutomationCommon.h"
 #include "UxtTestHandTracker.h"
@@ -15,170 +16,209 @@
 
 namespace
 {
-	AActor* CreateTestActor(UWorld* World, const FVector& Location)
+	const FVector TargetLocation(150, 0, 0);
+
+	UUxtGenericManipulatorComponent* CreateTestComponent(const FComponentReference& TargetComponent = FComponentReference())
 	{
+		UWorld* World = UxtTestUtils::GetTestWorld();
 		AActor* Actor = World->SpawnActor<AActor>();
 
-		USceneComponent* Root = NewObject<USceneComponent>(Actor);
-		Actor->SetRootComponent(Root);
-		Root->SetWorldLocation(Location);
-		Root->RegisterComponent();
-
-		UStaticMeshComponent* Mesh = UxtTestUtils::CreateBoxStaticMesh(Actor, FVector(0.3f));
-		Mesh->SetupAttachment(Actor->GetRootComponent());
-		Mesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-		Mesh->SetCollisionProfileName(TEXT("OverlapAll"));
-		Mesh->SetGenerateOverlapEvents(true);
+		// Box Mesh
+		UStaticMeshComponent* Mesh = UxtTestUtils::CreateBoxStaticMesh(Actor);
+		Actor->SetRootComponent(Mesh);
 		Mesh->RegisterComponent();
 
-		return Actor;
-	}
+		// Generic manipulator component
+		UUxtGenericManipulatorComponent* Manipulator = NewObject<UUxtGenericManipulatorComponent>(Actor);
+		Manipulator->TargetComponent = TargetComponent;
+		Manipulator->SetSmoothing(0.0f);
+		Manipulator->RegisterComponent();
 
-	UUxtGenericManipulatorComponent* CreateTestComponent(UWorld* World, const FVector& Location, const FComponentReference* TargetComponent = nullptr)
-	{
-		AActor* Actor = CreateTestActor(World, Location);
+		Actor->SetActorLocation(TargetLocation);
 
-		UUxtGenericManipulatorComponent* TestTarget = NewObject<UUxtGenericManipulatorComponent>(Actor);
-		if (TargetComponent)
-		{
-			TestTarget->TargetComponent = *TargetComponent;
-		}
-		TestTarget->RegisterComponent();
-
-		return TestTarget;
-	}
-
-	UGenericManipulatorTestComponent* CreateEventCaptureComponent(UUxtGenericManipulatorComponent* Manipulator)
-	{
-		UGenericManipulatorTestComponent* EventCaptureComponent = NewObject<UGenericManipulatorTestComponent>(Manipulator->GetOwner());
-		EventCaptureComponent->RegisterComponent();
-
-		Manipulator->OnUpdateTransform.AddDynamic(EventCaptureComponent, &UGenericManipulatorTestComponent::UpdateTransform);
-
-		return EventCaptureComponent;
+		return Manipulator;
 	}
 }
 
-BEGIN_DEFINE_SPEC(GenericManipulatorSpec, "UXTools.GenericManipulatorTest", EAutomationTestFlags::ProductFilter | EAutomationTestFlags::ApplicationContextMask)
+BEGIN_DEFINE_SPEC(GenericManipulatorSpec, "UXTools.GenericManipulator", EAutomationTestFlags::ProductFilter | EAutomationTestFlags::ApplicationContextMask)
 
-void EnqueueCorrectTargetTest();
+	// Core components (created by default)
+	UUxtGenericManipulatorComponent* Target;
+	FFrameQueue FrameQueue;
 
-UWorld* World;
-FVector Center;
-UUxtNearPointerComponent* Pointer;
-
-UUxtGenericManipulatorComponent* Manipulator;
-USceneComponent* TargetComponent;
-UGenericManipulatorTestComponent* EventCaptureComponent;
-
-FFrameQueue FrameQueue;
+	// Optional components (must be created by Describe block if needed)
+	UUxtNearPointerComponent* NearPointer;
+	UUxtFarPointerComponent* FarPointer;
 
 END_DEFINE_SPEC(GenericManipulatorSpec)
 
 void GenericManipulatorSpec::Define()
 {
-	Describe("GenericManipulator", [this]
+	BeforeEach([this]
+		{
+			TestTrueExpr(AutomationOpenMap(TEXT("/Game/UXToolsGame/Tests/Maps/TestEmpty")));
+
+			UWorld* World = UxtTestUtils::GetTestWorld();
+			FrameQueue.Init(&World->GetGameInstance()->GetTimerManager());
+
+			UxtTestUtils::EnableTestHandTracker();
+
+			Target = CreateTestComponent();
+		});
+
+	AfterEach([this]
+		{
+			Target->GetOwner()->Destroy();
+			Target = nullptr;
+
+			UxtTestUtils::DisableTestHandTracker();
+
+			FrameQueue.Reset();
+		});
+
+	Describe("Component Targeting", [this]
+		{
+			It("should default to parent actor", [this]
+				{
+					USceneComponent* ExpectedTarget = Target->GetOwner()->GetRootComponent();
+					TestEqual("Correct component targeted", Target->TransformTarget, ExpectedTarget);
+				});
+
+			It("should target user defined component (same actor)", [this]
+				{
+					FComponentReference TargetComponentReference;
+					TargetComponentReference.ComponentProperty = "UxtGenericManipulatorComponent_0";
+
+					UUxtGenericManipulatorComponent* ManipulatorComponent = CreateTestComponent(TargetComponentReference);
+					USceneComponent* ExpectedTarget = ManipulatorComponent->GetOwner()->FindComponentByClass<UUxtGenericManipulatorComponent>();
+					TestEqual("Correct component targeted", ManipulatorComponent->TransformTarget, ExpectedTarget);
+
+					ManipulatorComponent->GetOwner()->Destroy();
+				});
+
+			It("should target user defined component (different actor)", [this]
+				{
+					FComponentReference TargetComponentReference;
+					TargetComponentReference.OtherActor = Target->GetOwner();
+					TargetComponentReference.ComponentProperty = "UxtGenericManipulatorComponent_0";
+
+					UUxtGenericManipulatorComponent* ManipulatorComponent = CreateTestComponent(TargetComponentReference);
+					USceneComponent* ExpectedTarget = Target->GetOwner()->FindComponentByClass<UUxtGenericManipulatorComponent>();
+					TestEqual("Correct component targeted", ManipulatorComponent->TransformTarget, ExpectedTarget);
+
+					ManipulatorComponent->GetOwner()->Destroy();
+				});
+		});
+
+	Describe("Manipulation Events", [this]
 		{
 			BeforeEach([this]
 				{
-					// Load the empty test map to run the test in.
-					TestTrueExpr(AutomationOpenMap(TEXT("/Game/UXToolsGame/Tests/Maps/TestEmpty")));
-
-					World = UxtTestUtils::GetTestWorld();
-					FrameQueue.Init(&World->GetGameInstance()->GetTimerManager());
-
-					UxtTestUtils::EnableTestHandTracker();
-
-					Center = FVector(150, 0, 0);
-					Pointer = UxtTestUtils::CreateNearPointer(World, "TestPointer", Center + FVector(-15, 0, 0));
+					NearPointer = UxtTestUtils::CreateNearPointer(UxtTestUtils::GetTestWorld(), "Near Pointer", TargetLocation + FVector(-15, 0, 0));
 				});
 
 			AfterEach([this]
 				{
-					UxtTestUtils::DisableTestHandTracker();
-
-					FrameQueue.Reset();
-
-					if (Manipulator)
-					{
-						Manipulator->GetOwner()->Destroy();
-						Manipulator = nullptr;
-					}
-					Pointer->GetOwner()->Destroy();
-					Pointer = nullptr;
+					NearPointer->GetOwner()->Destroy();
+					NearPointer = nullptr;
 				});
 
-			LatentIt("should default to parent actor if no user defined target is set", [this](const FDoneDelegate& Done)
+			LatentIt("should raise transform update events", [this](const FDoneDelegate& Done)
 				{
-					Manipulator = CreateTestComponent(World, Center);
-					TargetComponent = Manipulator->GetOwner()->GetRootComponent();
+					UGenericManipulatorTestComponent* EventCaptureComponent = NewObject<UGenericManipulatorTestComponent>(Target->GetOwner());
+					Target->OnUpdateTransform.AddDynamic(EventCaptureComponent, &UGenericManipulatorTestComponent::UpdateTransform);
+					EventCaptureComponent->RegisterComponent();
 
-					EnqueueCorrectTargetTest();
-					FrameQueue.Enqueue([Done] { Done.Execute(); });
-				});
-
-			LatentIt("should use user defined component on same actor", [this](const FDoneDelegate& Done)
-				{
-					FComponentReference TargetComponentReference;
-					TargetComponentReference.ComponentProperty = "StaticMeshComponent_0";
-
-					Manipulator = CreateTestComponent(World, Center, &TargetComponentReference);
-					TargetComponent = Manipulator->GetOwner()->FindComponentByClass<UStaticMeshComponent>();
-
-					EnqueueCorrectTargetTest();
-					FrameQueue.Enqueue([Done] { Done.Execute(); });
-				});
-
-			LatentIt("should use user defined component on different actor", [this](const FDoneDelegate& Done)
-				{
-					AActor* TargetActor = CreateTestActor(World, Center);
-					FComponentReference TargetComponentReference;
-					TargetComponentReference.OtherActor = TargetActor;
-					TargetComponentReference.ComponentProperty = "StaticMeshComponent_0";
-
-					Manipulator = CreateTestComponent(World, Center, &TargetComponentReference);
-					TargetComponent = TargetActor->FindComponentByClass<UStaticMeshComponent>();
-
-					EnqueueCorrectTargetTest();
-					FrameQueue.Enqueue([Done] { Done.Execute(); });
-
-					TargetActor->Destroy();
-				});
-
-			LatentIt("should trigger transform update events", [this](const FDoneDelegate& Done)
-				{
-					Manipulator = CreateTestComponent(World, Center);
-					EventCaptureComponent = CreateEventCaptureComponent(Manipulator);
-
-					FrameQueue.Enqueue([this]()
+					FrameQueue.Enqueue([this]
 						{
 							UxtTestUtils::GetTestHandTracker().SetGrabbing(true);
 						});
-					FrameQueue.Enqueue([this]()
+
+					FrameQueue.Enqueue([this]
 						{
-							UxtTestUtils::GetTestHandTracker().SetAllJointPositions(Center + (FVector::RightVector * 10));
+							TestTrue("Component is grabbed", Target->GetGrabPointers().Num() > 0);
+							UxtTestUtils::GetTestHandTracker().SetAllJointPositions(FVector::RightVector * 10);
 						});
-					FrameQueue.Enqueue([this]()
+
+					FrameQueue.Enqueue([this, EventCaptureComponent]()
 						{
-							TestTrue("Transform update event triggered", EventCaptureComponent->TransformUpdateCount > 0);
+							TestTrue("Transform update events triggered", EventCaptureComponent->TransformUpdateCount > 0);
 						});
 
 					FrameQueue.Enqueue([Done] { Done.Execute(); });
 				});
 		});
-}
 
-void GenericManipulatorSpec::EnqueueCorrectTargetTest()
-{
-	FrameQueue.Enqueue([this]()
+	Describe("Far Interaction", [this]
 		{
-			UxtTestUtils::GetTestHandTracker().SetGrabbing(true);
-		});
+			BeforeEach([this]
+				{
+					FarPointer = UxtTestUtils::CreateFarPointer(UxtTestUtils::GetTestWorld(), "Far Pointer", TargetLocation + FVector(-200, 0, 0));
+				});
 
-	FrameQueue.Enqueue([this]()
-		{
-			TestTrue("Target grabbed", Manipulator->GetGrabPointers().Num() > 0);
-			TestTrue("Correct component targeted", Manipulator->TransformTarget == TargetComponent);
+			AfterEach([this]
+				{
+					FarPointer->GetOwner()->Destroy();
+					FarPointer = nullptr;
+				});
+
+			LatentIt("should rotate around center with one hand", [this](const FDoneDelegate& Done)
+				{
+					const FQuat ExpectedRotation(FVector::ForwardVector, FMath::DegreesToRadians(90));
+					const FTransform ExpectedTransform(ExpectedRotation, TargetLocation, FVector::OneVector);
+
+					FrameQueue.Enqueue([this]
+						{
+							Target->OneHandRotationMode = EUxtOneHandRotationMode::RotateAboutObjectCenter;
+							UxtTestUtils::GetTestHandTracker().SetSelectPressed(true);
+						});
+
+					FrameQueue.Enqueue([this, ExpectedRotation]
+						{
+							TestTrue("Component is grabbed", Target->GetGrabPointers().Num() > 0);
+							UxtTestUtils::GetTestHandTracker().SetAllJointOrientations(ExpectedRotation);
+						});
+
+					FrameQueue.Skip();
+
+					FrameQueue.Enqueue([this, ExpectedTransform]()
+						{
+							const FTransform Result = Target->GetOwner()->GetTransform();
+							TestTrue("Object is rotated", Result.Equals(ExpectedTransform));
+						});
+
+					FrameQueue.Enqueue([Done] { Done.Execute(); });
+				});
+
+			LatentIt("should rotate around grab point with one hand", [this](const FDoneDelegate& Done)
+				{
+					const FQuat ExpectedRotation(FVector::ForwardVector, FMath::DegreesToRadians(90));
+					const FVector ExpectedLocation(TargetLocation + FVector(0, 50, -50));
+					const FTransform ExpectedTransform(ExpectedRotation, ExpectedLocation, FVector::OneVector);
+
+					FrameQueue.Enqueue([this]
+						{
+							Target->OneHandRotationMode = EUxtOneHandRotationMode::RotateAboutGrabPoint;
+							UxtTestUtils::GetTestHandTracker().SetAllJointPositions(FVector(0, 50, 0));
+							UxtTestUtils::GetTestHandTracker().SetSelectPressed(true);
+						});
+
+					FrameQueue.Enqueue([this, ExpectedRotation]
+						{
+							TestTrue("Component is grabbed", Target->GetGrabPointers().Num() > 0);
+							UxtTestUtils::GetTestHandTracker().SetAllJointOrientations(ExpectedRotation);
+						});
+
+					FrameQueue.Skip();
+
+					FrameQueue.Enqueue([this, ExpectedTransform]()
+						{
+							const FTransform Result = Target->GetOwner()->GetTransform();
+							TestTrue("Object is rotated", Result.Equals(ExpectedTransform));
+						});
+
+					FrameQueue.Enqueue([Done] { Done.Execute(); });
+				});
 		});
 }
 
