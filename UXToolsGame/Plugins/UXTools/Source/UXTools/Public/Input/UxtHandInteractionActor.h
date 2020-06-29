62285f13bcef9ed407a98a549b9f8f450c3d464b
Carl McCaffrey <Carl.McCaffrey@microsoft.com>
2020-06-29 14:42:03 +0000

    Merged PR 380: Move "Show near cursor on grab targets" to advanced properties
    Targeting public 0.9.x
    
    Related work items: #504

--- UXToolsGame/Plugins/UXTools/Source/UXTools/Public/Input/UxtHandInteractionActor.h
+++ UXToolsGame/Plugins/UXTools/Source/UXTools/Public/Input/UxtHandInteractionActor.h
@@ -75,7 +75,7 @@ public:
 	bool bUseDefaultFarBeam = true;
 
 	/** Show the near cursor on grab targets. Changes to this value after BeginPlay have no effect. */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hand Interaction", meta = (ExposeOnSpawn = true))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, AdvancedDisplay, Category = "Hand Interaction", meta = (ExposeOnSpawn = true))
 	bool bShowNearCursorOnGrabTargets = false;
 
 private:
