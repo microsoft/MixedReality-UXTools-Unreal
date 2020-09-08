e7416a7816d82f2c9b9e38061e247bf2c4272c4e
Lukas Tönne <lutnne@microsoft.com>
2020-09-08 15:32:35 +0200

    Cleanup input sim LocalPlayerSubsytem.
    * Move view target initialization into the CreateHmdCameraActor function (it applies only to HmdCameraActor)
    * Create actors in PostLogin rather than in the initialization

--- UXToolsGame/Plugins/UXTools/Source/UXToolsInputSimulation/Private/UxtInputSimulationLocalPlayerSubsystem.cpp
+++ UXToolsGame/Plugins/UXTools/Source/UXToolsInputSimulation/Private/UxtInputSimulationLocalPlayerSubsystem.cpp
@@ -32,43 +32,18 @@ void UUxtInputSimulationLocalPlayerSubsystem::Initialize(FSubsystemCollectionBas
 {
 	SimulationState = NewObject<UUxtInputSimulationState>();
 
-	// Subscribe to PostLoadMap event to recreate the actors after a map has been destroyed.
-	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &UUxtInputSimulationLocalPlayerSubsystem::OnPostLoadMapWithWorld);
-
 	// Subscribe to the PostLogin event to set the view target after the local player controller is created.
 	FGameModeEvents::GameModePostLoginEvent.AddUObject(this, &UUxtInputSimulationLocalPlayerSubsystem::OnGameModePostLogin);
+	// Destroy actors after logout
+	FGameModeEvents::GameModeLogoutEvent.AddUObject(this, &UUxtInputSimulationLocalPlayerSubsystem::OnGameModeLogout);
 
-	if (UWorld* World = GetWorld())
-	{
-		CreateActors(World);
-	}
+	// Subscribe to PostLoadMap event to recreate the actors after a map has been destroyed.
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &UUxtInputSimulationLocalPlayerSubsystem::OnPostLoadMapWithWorld);
 }
 
 void UUxtInputSimulationLocalPlayerSubsystem::Deinitialize()
 {
-	DestroyInputSimActor();
-	DestroyHmdCameraActor();
-
 	SimulationState = nullptr;
-}
-
-void UUxtInputSimulationLocalPlayerSubsystem::CreateActors(UWorld* World)
-{
-	if (World && World->IsPlayInEditor())
-	{
-		CreateInputSimActor(World);
-		CreateHmdCameraActor(World);
-
-		ULocalPlayer* Player = GetLocalPlayer();
-		if (Player)
-		{
-			APlayerController* PC = Player->GetPlayerController(World);
-			if (PC)
-			{
-				SetPlayerCameraTarget(PC);
-			}
-		}
-	}
 }
 
 void UUxtInputSimulationLocalPlayerSubsystem::CreateInputSimActor(UWorld* World)
@@ -105,6 +80,23 @@ void UUxtInputSimulationLocalPlayerSubsystem::CreateHmdCameraActor(UWorld* World
 
 		HmdCameraActorWeak = HmdCameraActor;
 	}
+
+	// Set the HmdCameraActor as the view target for the player controller
+	if (AActor* HmdCameraActor = HmdCameraActorWeak.Get())
+	{
+		ULocalPlayer* Player = GetLocalPlayer();
+		if (Player)
+		{
+			APlayerController* PlayerController = Player->GetPlayerController(World);
+			if (PlayerController)
+			{
+				// Set the view target for the player controller to render from the HMD position
+				PlayerController->SetViewTarget(HmdCameraActor);
+				// Prevent the controller from resetting the view target when the player is restarted
+				PlayerController->bAutoManageActiveCameraTarget = false;
+			}
+		}
+	}
 }
 
 void UUxtInputSimulationLocalPlayerSubsystem::DestroyInputSimActor()
@@ -125,29 +117,31 @@ void UUxtInputSimulationLocalPlayerSubsystem::DestroyHmdCameraActor()
 	HmdCameraActorWeak.Reset();
 }
 
-void UUxtInputSimulationLocalPlayerSubsystem::SetPlayerCameraTarget(APlayerController* PlayerController)
-{
-	check(PlayerController);
-	if (AActor* HmdCameraActor = HmdCameraActorWeak.Get())
-	{
-		// Set the view target for the player controller to render from the HMD position
-		PlayerController->SetViewTarget(HmdCameraActor);
-		// Prevent the controller from resetting the view target when the player is restarted
-		PlayerController->bAutoManageActiveCameraTarget = false;
-	}
-}
-
 void UUxtInputSimulationLocalPlayerSubsystem::OnGameModePostLogin(AGameModeBase* GameMode, APlayerController* NewPlayer)
 {
 	if (NewPlayer->Player == GetLocalPlayer())
 	{
-		SetPlayerCameraTarget(NewPlayer);
+		CreateInputSimActor(GetWorld());
+		CreateHmdCameraActor(GetWorld());
+	}
+}
+
+void UUxtInputSimulationLocalPlayerSubsystem::OnGameModeLogout(AGameModeBase* GameMode, AController* Exiting)
+{
+	if (APlayerController* PlayerController = Cast<APlayerController>(Exiting))
+	{
+		if (PlayerController->Player == GetLocalPlayer())
+		{
+			DestroyInputSimActor();
+			DestroyHmdCameraActor();
+		}
 	}
 }
 
 void UUxtInputSimulationLocalPlayerSubsystem::OnPostLoadMapWithWorld(UWorld* LoadedWorld)
 {
-	CreateActors(LoadedWorld);
+	CreateInputSimActor(LoadedWorld);
+	CreateHmdCameraActor(LoadedWorld);
 }
 
 #undef LOCTEXT_NAMESPACE
