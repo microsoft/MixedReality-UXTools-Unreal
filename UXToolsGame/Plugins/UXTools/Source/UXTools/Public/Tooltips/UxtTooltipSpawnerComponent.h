0a74f3e70723faf638281cffcd5f0959de043b02
Lukas Tönne <lutnne@microsoft.com>
2020-09-11 11:23:31 +0200

    Make the pivot component reference editable.

--- UXToolsGame/Plugins/UXTools/Source/UXTools/Public/Tooltips/UxtTooltipSpawnerComponent.h
+++ UXToolsGame/Plugins/UXTools/Source/UXTools/Public/Tooltips/UxtTooltipSpawnerComponent.h
@@ -164,7 +164,7 @@ private:
 	FText TooltipText;
 
 	/** An offset to specify where the tooltip will be spawned. */
-	UPROPERTY(VisibleAnywhere, Category = "Tooltip", meta=(UseComponentPicker, AllowedClasses="SceneComponent"))
+	UPROPERTY(EditAnywhere, Category = "Tooltip", meta=(UseComponentPicker, AllowedClasses="SceneComponent"))
 	FComponentReference Pivot;
 
 	/** Overrides the auto anchor on the spawned tooltip. */
