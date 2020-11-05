204df552fa91a4b5b5c94d31070a6cb4063a975a
Lukas Tönne <lutnne@microsoft.com>
2020-11-05 18:14:25 +0100

    Remove the MRPlatExt plugin from the repo, this should be installed by the user.

--- UXToolsGame/Plugins/MRPlatExt/Source/MRPlatExtRuntimeSettings/Private/MRPlatExtRuntimeSettingsModule.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-
-#include "Modules/ModuleInterface.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_MODULE(FDefaultModuleImpl, MRPlatExtRuntimeSettings);
