c8da2565b3d331130f69d4f5c0829339e8548ccf
Maciej Borowik <maboro@microsoft.com>
2020-11-10 10:50:41 +0000

    Merged PR 618: Display current git commit in the Loader scene (packaged game only)
    Related work items: #811

---
+++ UXToolsGame/Plugins/UXToolsExamples/Source/UXToolsExamples/Private/UxtExamplesInfo.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2020 Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "UxtExamplesInfo.h"
+
+#include "UxtExamplesVersion.h"
+
+#include "Internationalization/Text.h"
+
+FText UUxtExamplesInfo::CommitSHA(const UObject* WorldContext)
+{
+	// VCS information can only be reliably set in packaged projects
+	if (WorldContext && (WorldContext->GetWorld()->WorldType == EWorldType::Game))
+	{
+		return FText::FromString(TEXT(UXTOOLSEXAMPLES_COMMIT_SHA));
+	}
+	return FText();
+}
