2a0a5fb58ec23081ef8be7ecc7d4b63c2ae657cc
Julian Jimenez Gonzalez <jujime@microsoft.com>
2020-12-02 18:44:41 +0000

    Merged PR 628: Implement min/max scale constraint
    Adds implementation for the component and adds one instance to each actor in the `BoundsControlExample` scene, both of them configured with `0.3/5.0/RelativeToInitialTransform`.

---
+++ UXToolsGame/Plugins/UXTools/Source/UXTools/Public/Interactions/Constraints/UxtMinMaxScaleConstraint.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2020 Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Interactions/Constraints/UxtTransformConstraint.h"
+
+#include "UxtMinMaxScaleConstraint.generated.h"
+
+/**
+ * Constraint that limits the min/max scale.
+ */
+UCLASS(ClassGroup = UXTools, meta = (BlueprintSpawnableComponent))
+class UXTOOLS_API UUxtMinMaxScaleConstraint : public UUxtTransformConstraint
+{
+	GENERATED_BODY()
+public:
+	virtual EUxtTransformMode GetConstraintType() const;
+
+	virtual void ApplyConstraint(FTransform& Transform) const;
+
+	virtual void Initialize(const FTransform& WorldPose);
+
+	/** Minimum scale allowed */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Constraints, meta = (Bitmask, BitmaskEnum = EUxtAxisFlags))
+	float MinScaleFactor = 0.0f;
+
+	/** Maximum scale allowed */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Constraints, meta = (Bitmask, BitmaskEnum = EUxtAxisFlags))
+	float MaxScaleFactor = 0.0f;
+
+	/** Whether the min/max values should be relative to the initial transform or not. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Constraints)
+	bool bRelativeToInitialTransform = false;
+
+private:
+	/** By using those vectors, the scales to apply aren't recalculated on each apply call. */
+	FVector MinScaleVec;
+	FVector MaxScaleVec;
+};
